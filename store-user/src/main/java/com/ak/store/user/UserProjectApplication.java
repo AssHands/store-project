d468770f9a77cb61919821c576c9c1bac730c957
Artem Khailenko <umafamilia19@gmail.com>
2024-10-23 17:06:33 +0300

    new user module

---
+++ store-user/src/main/java/com/ak/store/user/UserProjectApplication.java
@@ -0,0 +1,11 @@
+package com.ak.store.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserProjectApplication.class, args);
+    }
+}
