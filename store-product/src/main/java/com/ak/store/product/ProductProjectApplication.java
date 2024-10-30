00a950133a549d45b8b36e53bb0dd49c08902e49
Artem Khailenko <umafamilia19@gmail.com>
2024-10-30 15:30:27 +0300

    commit for pull

--- store-product/src/main/java/com/ak/store/product/ProductProjectApplication.java
+++ store-product/src/main/java/com/ak/store/product/ProductProjectApplication.java
@@ -3,8 +3,14 @@ package com.ak.store.product;
 import com.ak.store.filterQuery.FilterQueryGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.ak.store.*")
+@ComponentScan(basePackages = {"com.ak.store.*" })
+@EntityScan("com.ak.store.*")
 @SpringBootApplication
 public class ProductProjectApplication {
     public static void main(String[] args) {
