1cfb1e20d8180dd3950549dca5624f235c941ce2
Artem Khailenko <umafamilia19@gmail.com>
2024-12-25 22:06:17 +0300

    add logstash to docker-compose.yaml

--- store-catalogue/src/main/java/com/ak/store/catalogue/service/ElasticService.java
+++ store-catalogue/src/main/java/com/ak/store/catalogue/service/ElasticService.java
@@ -1,10 +1,8 @@
 package com.ak.store.catalogue.service;
 
-import ch.qos.logback.core.util.StringUtil;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.FieldValue;
 import co.elastic.clients.elasticsearch._types.SortOrder;
-import co.elastic.clients.elasticsearch._types.aggregations.Aggregate;
 import co.elastic.clients.elasticsearch._types.aggregations.Aggregation;
 import co.elastic.clients.elasticsearch._types.aggregations.AggregationRange;
 import co.elastic.clients.elasticsearch._types.query_dsl.*;
