efb8c5b607ea95a1d50e91840d40e7ce38bfca10
Artem Khailenko <umafamilia19@gmail.com>
2025-03-31 12:41:30 +0300

    make review microservice

---
+++ store-review/src/main/java/com/ak/store/review/ReviewProjectApplication.java
@@ -0,0 +1,11 @@
+package com.ak.store.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewProjectApplication.class, args);
+    }
+}
---
+++ store-review/src/main/java/com/ak/store/review/facade/ReviewFacade.java
@@ -0,0 +1,45 @@
+package com.ak.store.review.facade;
+
+import com.ak.store.common.model.consumer.form.CommentForm;
+import com.ak.store.common.model.consumer.form.ReviewForm;
+import com.ak.store.common.model.consumer.view.CommentView;
+import com.ak.store.common.model.consumer.view.ReviewView;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ReviewFacade {
+    public List<ReviewView> findAllReviewByProductId(Long productId) {
+
+    }
+
+    public List<CommentView> findAllCommentByReviewId(Long ReviewId) {
+
+    }
+
+    public Long createOneReview(String consumerId, Long productId, ReviewForm reviewForm) {
+
+    }
+
+    public Long updateOneReview(Long reviewId, ReviewForm reviewForm) {
+    }
+
+    public void deleteOneReview(Long reviewId) {
+    }
+
+    public Long createOneComment(String consumerId, Long reviewId, CommentForm commentForm) {
+    }
+
+    public Long updateOneComment(Long commentId, CommentForm commentForm) {
+    }
+
+    public void deleteOneComment(Long commentId) {
+    }
+
+    public void deleteAllByProductId(Long productId) {
+    }
+
+}
